046bbb751ea005ebf901e39274ec3e6a7b07c403
twtyygypl <35924310+twtyygypl@users.noreply.github.com>
2025-01-12 06:10:44 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
-# diy-part1.sh 脚本（用于自定义设置和修改）
+# diy-part1.sh 脚本(用于自定义设置和修改)
 
 # diy-part1.sh 通常用于做一些全局性的设置，比如添加默认包、修改内核配置、修改主机名等。
 --------------------------------------------------------
@@ -6,7 +6,7 @@
 #!/bin/bash
 
 # 修改默认主机名
-# sed -i 's/xxxxx/OpenWrt/g' package/base-files/files/bin/config_generate
+# sed -i 's/immortalwrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
